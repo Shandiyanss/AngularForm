bfd1379dc0a8889ac7b757cc6d4ad0622be27304
Shandiyanss <210701240@rajalakshmi.edu.in>
2025-06-24 11:51:33 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { App } from './app'; 
+import { Info } from './info/info'; 
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: App },
+  { path: 'info', component: Info}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+   imports: [FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'Form';
+export class App{
+  //constructor(private router: Router) {}
+  UserObj: any={
+    Mobilenumber:' ',
+    Relationship:' ',
+    salutation:' ',
+    Patientname:' ',
+    PatientSurname:' ',
+    Age:' ',
+    DateofBirth:' ',
+    Gender:' ',
+    SelectionPrefix:' ',
+    Relationname:' ',
+    Alternatenumber:' ',
+    Address:' ',
+    Pincode:' ',
+    city:' ',
+    Email:' ',
+  };
+
+  today = new Date().toISOString().split('T')[0];
+
+  onSubmit(form: any) {
+  if (form.valid) {
+    console.log('Form Submitted Successfully');
+    console.log(this.UserObj);  // You can send this to API or display it
+  } else {
+    console.log('Form is invalid. Please fill all required fields correctly.');
+  }
 }
+
+
+  }
